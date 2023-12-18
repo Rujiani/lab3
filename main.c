cbc4fbdcd8e23bf7c3e2efc8b2043373fac70de4
Rujian <roman.2006.08.18@gmail.com>
2023-12-18 16:49:39 +0300

    make main.c

---
+++ main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+int main(){
+    int input;
+    do{
+        printf("1 Create array\n");
+        printf("2 Add new number\n");
+        printf("3 Delete number\n");
+        printf("4 Create progressive array\n");
+        printf("5 Output array\n");
+        printf("6 Output progressive array\n");
+        printf("7 Exit\n");
+        scanf("%d", &input);
+        switch (input){
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 5:
+                break;
+            case 6:
+                break;
+            case 7:
+                break;
+            default:
+                break;
+        }
+    }
+    while(input != 7);
+    return 0;
+}
